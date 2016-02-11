5dfa28ded3676465968ae2beec615cf30494c7fb
Michael Singh <michael@mike-singh.com>
2016-02-11 00:29:36 -0500

    setting up basic fullscreen

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 var Application = {};
 window.Application = Application;
 
-function UserInterface(assets, imageId, buttonClass) {
+function UserInterface(assets, imageId, buttonClass, fullScreenButtonId) {
   this.assets = assets;
   this.imageId = imageId;
   this.imageNode = document.getElementById(this.imageId);
   this.count = 0;
   this.buttonClass = buttonClass;
   this.buttons = document.getElementsByClassName(buttonClass);
+  this.isFullscreen = false;
+  this.fullScreenButton = document.getElementById(fullScreenButtonId);
+  this.cancelButton = document.getElementsByClassName('cancel-fullscreen')[0];
 }
 
 UserInterface.prototype.selectImage = function (imageSrc, button) {
@@ -23,17 +26,57 @@ UserInterface.prototype.removeHighlightClasses = function () {
   }
 };
 
+UserInterface.prototype.toggleFullScreen = function () {
+  var fullScreenHeader = document.getElementsByClassName('full-screen-header')[0];
+  var phoneInfo = document.getElementsByClassName('phone-info')[0];
+  var mainHeader = document.getElementsByClassName('main-header')[0];
+  var cancelButton = document.getElementsByClassName('cancel-fullscreen')[0];
+  var phoneDisplay = document.getElementsByClassName('phone-display')[0];
+  //components to hide when full screen  link is clicked
+  var uiComponents = [
+    mainHeader,
+    phoneInfo,
+    logo,
+    Application.ui.fullScreenButton
+  ];
+
+  this.isFullscreen = !this.isFullscreen;
+
+  if (this.isFullscreen) {
+    fullScreenHeader.style.display = 'block';
+    uiComponents.forEach(function(component) {
+        component.style.display = 'none';
+        phoneDisplay.style.width = '100%';
+    });
+  } else {
+    fullScreenHeader.style.display = 'none';
+    uiComponents.forEach(function(component) {
+        component.style.display = 'block';
+        phoneDisplay.style.width = '50%';
+    });
+  }
+};
+
 
 var assets = [
   './assets/phone-gray-select.png',
   './assets/phone-silver-select.png'
 ];
-Application.ui = new UserInterface(assets, 'iphone-display', 'color-choice');
 
-Application.ui.buttons[0].addEventListener("click", function () {
+Application.ui = new UserInterface(assets, 'iphone-display', 'color-choice', 'toggle-full-screen');
+
+Application.ui.fullScreenButton.addEventListener('click', function () {
+  Application.ui.toggleFullScreen();
+});
+
+Application.ui.cancelButton.addEventListener('click', function () {
+  Application.ui.toggleFullScreen();
+});
+
+Application.ui.buttons[0].addEventListener('click', function () {
   Application.ui.selectImage(Application.ui.assets[0], this);
 });
 
-Application.ui.buttons[1].addEventListener("click", function () {
+Application.ui.buttons[1].addEventListener('click', function () {
   Application.ui.selectImage(Application.ui.assets[1], this);
 });
