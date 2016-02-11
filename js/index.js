960d774c63cbd94387ed13699beaa713faf21e46
Michael Singh <michael@mike-singh.com>
2016-02-10 21:57:06 -0500

    adding functionality to control button highlight toggling

--- js/index.js
+++ js/index.js
@@ -6,12 +6,23 @@ function UserInterface(assets, imageId, buttonClass) {
   this.imageId = imageId;
   this.imageNode = document.getElementById(this.imageId);
   this.count = 0;
+  this.buttonClass = buttonClass;
   this.buttons = document.getElementsByClassName(buttonClass);
 }
 
-UserInterface.prototype.selectImage = function (imageSrc) {
+UserInterface.prototype.selectImage = function (imageSrc, button) {
+  this.removeHighlightClasses();
   this.imageNode.src = imageSrc;
+  button.className += ' chosen';
 };
+
+UserInterface.prototype.removeHighlightClasses = function () {
+  for (var index = 0; index < this.buttons.length; index++) {
+    var button = this.buttons[index];
+    button.className = this.buttonClass;
+  }
+};
+
 
 var assets = [
   './assets/phone-gray-select.png',
@@ -20,9 +31,9 @@ var assets = [
 Application.ui = new UserInterface(assets, 'iphone-display', 'color-choice');
 
 Application.ui.buttons[0].addEventListener("click", function () {
-  Application.ui.selectImage(Application.ui.assets[0]);
+  Application.ui.selectImage(Application.ui.assets[0], this);
 });
 
 Application.ui.buttons[1].addEventListener("click", function () {
-  Application.ui.selectImage(Application.ui.assets[1]);
+  Application.ui.selectImage(Application.ui.assets[1], this);
 });
